0d8dd9ace5c676db58aa19037f01ee640011de72
Reyhana Izzal muttaqim <izzalm.008@gmail.com>
2026-01-27 11:20:21 +0700

    fix: remove button save

--- resources/views/admin/pages/pool/index.blade.php
+++ resources/views/admin/pages/pool/index.blade.php
@@ -70,15 +70,6 @@
                     class="flex-1 md:flex-none justify-center px-4 py-2 bg-white border border-gray-300 text-gray-700 rounded-lg hover:bg-gray-50 flex items-center gap-2 text-sm font-medium transition">
                     Live Preview
                 </a>
-                <button
-                    class="flex-1 md:flex-none justify-center px-4 py-2 bg-accent text-white rounded-lg hover:bg-[#01579B] flex items-center gap-2 text-sm font-medium shadow-lg transition">
-                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4">
-                        </path>
-                    </svg>
-                    Update Status
-                </button>
             </div>
         </div>
 
