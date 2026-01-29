efbdecbd6c6ddd5e35fbf70bc824c164aa41850d
Reyhana Izzal muttaqim <izzalm.008@gmail.com>
2026-01-29 08:42:40 +0700

    fix: update color delete

--- resources/views/admin/pages/restaurant/panes/gallery.blade.php
+++ resources/views/admin/pages/restaurant/panes/gallery.blade.php
@@ -20,7 +20,7 @@
                     <div
                         class="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition flex items-center justify-center gap-2">
                         <button @click="deleteGalleryImage('{{ $image->id }}')"
-                            class="p-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
+                            class="p-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition">
                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
